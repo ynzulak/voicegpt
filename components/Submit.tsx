aa608023c08e4a46ae81d94d7ed0b7949c3b915b
[ynzulak] <maksymilian.a.kaluzny@gmail.com>
2023-10-20 21:40:59 +0200

    Added handling on enter and working on another component

--- components/Submit.tsx
+++ components/Submit.tsx
@@ -1,23 +1,30 @@
+import  inputResponse  from "./inputResponse";
 import { FontAwesomeIcon } from "../node_modules/@fortawesome/react-fontawesome/index";
 import { faCircleRight } from "../node_modules/@fortawesome/free-solid-svg-icons/index";
 
-import inputResponse  from "./inputResponse";
 
 const Submit = () => {
-
   const { inputMessage, setInputMessage, responseMessage, setResponseMessage, handleMessageSubmit } = inputResponse();
-    return (
-        <div className='user-container'>
+  
+  const handleKeyPress = (event: any) => {
+    if (event.key === 'Enter') {
+      handleMessageSubmit();
+    }
+  };
+
+  return (
+    <div className='user-container'>
         <div className='type-bar'>
           <div className='user-text'>
-            <input className='user-text' type='text' placeholder='Start typing...' value={inputMessage} onChange={(e) => setInputMessage(e.target.value)}/>
+            <input className='user-text' type='text' placeholder='Start typing...' value={inputMessage} onChange={(e) => setInputMessage(e.target.value)}
+            onKeyPress={handleKeyPress}/>
           </div>
           <div className='submit' onClick={handleMessageSubmit}><FontAwesomeIcon icon={faCircleRight} className='submit'/></div>
         </div>
       </div>
         
-    );
-    
+        );
+        
 }
 
 
